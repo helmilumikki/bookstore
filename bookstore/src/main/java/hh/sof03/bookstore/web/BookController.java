10cf2a59d7305ea1dd4dfa1bc57dc1b8367861b4
Helmi H <hyvonen.helmi@outlook.com>
2023-09-20 14:47:43 +0300

    added create and delete

--- bookstore/src/main/java/hh/sof03/bookstore/web/BookController.java
+++ bookstore/src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -3,10 +3,13 @@ package hh.sof03.bookstore.web;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import hh.sof03.bookstore.domain.Book;
 import hh.sof03.bookstore.domain.BookRepository;
+
 
 @Controller
 public class BookController {
@@ -25,4 +28,27 @@ public class BookController {
 		return "booklist";
 	}
 	
+	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+    public String deleteBook(@PathVariable("id") Long bookId, Model model) {
+    	repository.deleteById(bookId);
+        return "redirect:../booklist";
+    }  
+	
+	@RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String editBook(@PathVariable("id") Long bookId, Model model){
+    	model.addAttribute(repository.findById(bookId));
+        return "editbook";
+    }
+ 
+	@RequestMapping(value = "/add")
+    public String addBook(Model model){
+    	model.addAttribute("book", new Book());
+        return "addbook";
+    }     
+	
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+    public String save(Book book){
+        repository.save(book);
+        return "redirect:booklist";
+    }    
 }

f60609a06fca602f3a6f54a0169c85ed43be694f
helmilumikki <hyvonen.helmi@outlook.com>
2023-10-12 08:20:21 +0300

    db users

--- bookstore/src/main/java/hh/sof03/bookstore/BookstoreApplication.java
+++ bookstore/src/main/java/hh/sof03/bookstore/BookstoreApplication.java
@@ -9,6 +9,8 @@ import hh.sof03.bookstore.domain.Book;
 import hh.sof03.bookstore.domain.BookRepository;
 import hh.sof03.bookstore.domain.Category;
 import hh.sof03.bookstore.domain.CategoryRepository;
+import hh.sof03.bookstore.domain.User;
+import hh.sof03.bookstore.domain.UserRepository;
 
 @SpringBootApplication
 public class BookstoreApplication {
@@ -18,7 +20,7 @@ public class BookstoreApplication {
 	}
 
 	@Bean
-	public CommandLineRunner demo(BookRepository bookRepo, CategoryRepository catRepo) {
+	public CommandLineRunner demo(BookRepository bookRepo, CategoryRepository catRepo, UserRepository userRepo) {
 	return (args) -> {
 		
 		Category c1 = new Category("fantasy"); 
@@ -31,6 +33,12 @@ public class BookstoreApplication {
 		
 		bookRepo.save(new Book("The Hobbit", "J.R.R. Tolkien", "9780547928241", 1937, 25.0, c1));
 		bookRepo.save(new Book("Frankenstein", "Mary Shelley", "9780141393391", 1818, 25.0, c2));
+		
+		User user1 = new User("user", "$2a$06$3jYRJrg0ghaaypjZ/.g4SethoeA51ph3UD4kZi9oPkeMTpjKU5uo6", "USER", "user@mail.com");
+		User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN", "admin@admin.com");
+		userRepo.save(user1);
+		userRepo.save(user2);
+		
 		};
 	}
 
--- bookstore/src/main/java/hh/sof03/bookstore/web/BookController.java
+++ bookstore/src/main/java/hh/sof03/bookstore/web/BookController.java
@@ -1,6 +1,7 @@
 package hh.sof03.bookstore.web;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -32,12 +33,14 @@ public class BookController {
 	}
 	
 	@RequestMapping(value = "/delete/{id}", method = RequestMethod.GET)
+	@PreAuthorize("hasAuthority('ADMIN')")
     public String deleteBook(@PathVariable("id") Long bookId, Model model) {
     	repository.deleteById(bookId);
         return "redirect:../booklist";
     }  
 
 	@RequestMapping(value = "/edit/{id}")
+	@PreAuthorize("hasAuthority('ADMIN')")
 	public String editBook(@PathVariable("id") Long bookId, Model model){
 	model.addAttribute("book", repository.findById(bookId));
 	model.addAttribute("categories", catRepo.findAll());
@@ -57,8 +60,4 @@ public class BookController {
         return "redirect:booklist";
     }
 	
-	@RequestMapping(value="/login")
-    public String login() {	
-        return "login";
-    }
 }
